73df7387a16314fe657a43774ad0039aca03e8f1
Pablo Delgado Krämer <private@pablode.com>
2023-06-08 22:58:22 +0200

    Add support for GLSL_NV_shader_invocation_reorder

--- spirv_reflect.c
+++ spirv_reflect.c
@@ -777,6 +777,7 @@ static SpvReflectResult ParseNodes(SpvReflectPrvParser* p_parser)
       case SpvOpTypePipe:
       case SpvOpTypeAccelerationStructureKHR:
       case SpvOpTypeRayQueryKHR:
+      case SpvOpTypeHitObjectNV:
       {
         CHECKED_READU32(p_parser, p_node->word_offset + 1, p_node->result_id);
         p_node->is_type = true;
